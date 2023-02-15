94e43f5df6020044bbf01c66be0c0ed8cc0300dc
ddavidel <58525106+ddavidel@users.noreply.github.com>
2023-02-15 20:15:33 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system("if EXIST config.ini call ServerCreator.py")
-os.system("pip install wget")
-os.system("echo configured>config.ini")
-import wget
-url = ""
-wget.download(url)
-
-os.system("call ServerCreator.py")
