7af19533f6ba17d1cb6af78b9f95fb9add8c450d
Davide <dvdlavezzari@gmail.com>
2024-11-23 21:30:25 +0100

    feat: server detail page

--- modules/pages.py
+++ modules/pages.py
@@ -2,10 +2,17 @@
 Pages
 """
 
-from nicegui import ui, app
+from nicegui import ui, html, app
 
-from modules.utils import popup_create_server, create_server_card
-from modules.server import server_list
+from modules.utils import (
+    popup_create_server,
+    popup_edit_server
+)
+from modules.server import (
+    MinecraftServer,
+    server_list,
+    get_server_by_name
+)
 
 
 def build_base_window(header: ui.header):
@@ -14,6 +21,8 @@ def build_base_window(header: ui.header):
     # TODO: add here other css files.
     with header:
         ui.button("", on_click=app.shutdown, icon="close").classes("close-button")
+
+    ui.colors(primary="#13c187")
 
 def build_drawer():
     """Builds left drawer"""
@@ -34,6 +43,71 @@ def build_drawer():
             on_click=None,
             icon="terminal",
         ).classes("drawer-button")
+
+@ui.page('/server_detail/{server_name}')
+def server_detail(server_name: str):
+    """Page that displays the server details"""
+    # setup content
+    header = ui.header().classes("content-header")
+    container = html.section()
+    build_base_window(header=header)
+
+    server = get_server_by_name(server_name=server_name)
+    if not server:
+        # no server found.
+        pass
+
+    else:
+        with header:
+            ui.button("", on_click=app.shutdown, icon="close").classes("close-button")
+            ui.button("", on_click=ui.navigate.back, icon="arrow_back_ios_new").classes(
+                "back-button"
+            )
+            ui.label(server.name).style("font-size: 40px;")
+
+        with ui.left_drawer(top_corner=True, fixed=True).classes("left-drawer"):
+            ui.label("Settings").style("font-size: 35px")
+            ui.button(
+                "Console",
+                on_click=None,
+                icon="terminal",
+            ).classes("drawer-button")
+            ui.button(
+                "Edit server settings",
+                on_click=popup_edit_server(server=server).open,
+                icon="tune",
+            ).classes("drawer-button")
+            ui.button(
+                "Edit server properties",
+                on_click=None,
+                icon="edit_note",
+            ).classes("drawer-button")
+
+        with container:
+            ui.label("logs")
+
+def create_server_card(server: MinecraftServer):
+    """Create a server card for server"""
+    with ui.card().classes("server-card"):
+        with ui.row():
+            # ui.label(server.name).style("font-size: 25px")
+            # ui.link(server.name, target=f"/server_detail/{server.name}")
+            ui.link(server.name, f"/server_detail/{server.name}").classes("server-card-link")
+        with ui.row():
+            with ui.button_group():
+                ui.button(icon="play_arrow").on_click(server.start).classes(
+                    "start-button"
+                ).bind_enabled_from(server, "running", lambda s: not s)
+
+                ui.button(icon="stop").on_click(server.stop).classes(
+                    "stop-button"
+                ).bind_enabled_from(server, "running")
+
+            ui.label("").style(
+                "opacity: 0.6; margin-left: 200px; margin-top: 10px;"
+            ).bind_text_from(
+                server, "status", backward=lambda value: f"Status: {value}"
+            )
 
 
 @ui.refreshable
