d415f237b5550edf183880323ad6a003a646ef7a
Davide <dvdlavezzari@gmail.com>
2024-11-30 12:31:09 +0100

    feat(editor): server.properties loader

--- modules/server.py
+++ modules/server.py
@@ -49,6 +49,7 @@ class MinecraftServer:
     settings = binding.BindableProperty()
 
     def __init__(self, settings: dict, uuid: str = ""):
+        # Setup attributes
         self.name = settings.get("name")
         self.settings = settings or {}
         self.running = False
@@ -56,6 +57,7 @@ class MinecraftServer:
         self.stopping = False
         self.process = None
         self.log = None
+        self.server_properties = {}
 
         if not uuid:
             self._create_server()
@@ -131,9 +133,7 @@ class MinecraftServer:
     @property
     def has_server_properties(self) -> bool:
         """Returns true if server.properties is in server dir"""
-        return os.path.exists(
-            os.path.join(self.server_path, "server.properties")
-        )
+        return os.path.exists(os.path.join(self.server_path, "server.properties"))
 
     def _create_server(self):
         """
@@ -156,7 +156,11 @@ class MinecraftServer:
         self._download_jar()
 
         # accept eula
-        with open(os.path.join(self.settings["folder_path"], "eula.txt"), "w") as eula:
+        with open(
+            os.path.join(self.settings["folder_path"], "eula.txt"),
+            mode="w",
+            encoding="utf-8",
+        ) as eula:
             eula.write("eula=true")
 
         # save into server.json
@@ -315,7 +319,7 @@ class MinecraftServer:
                     elif os.path.isdir(item_path):
                         shutil.rmtree(item_path)
                 except Exception as e:
-                    print('Failed to delete %s: %s' % (item_path, e))
+                    print(f"Failed to delete {item_path}: {e}")
             # remove server dir
             shutil.rmtree(self.server_path)
 
@@ -336,6 +340,30 @@ class MinecraftServer:
             raise
 
         print(f"Deleted server {self.uuid}")
+
+    def load_server_properties(self):
+        """Loads server.properties file"""
+        if self.has_server_properties:
+            with open(
+                os.path.join(self.server_path, "server.properties"),
+                mode="r",
+                encoding="utf-8",
+            ) as properties:
+                for line in properties:
+                    if line.strip() and not line.startswith("#"):
+                        key, value = line.strip().split("=", 1)
+                        self.server_properties[key] = value
+
+    def save_server_properties(self):
+        """Saves server.properties file"""
+        raise NotImplementedError()
+        if self.has_server_properties and self.server_properties is not None:
+            with open(
+                os.path.join(self.server_path, "server.properties"),
+                mode="w",
+                encoding="utf-8",
+            ) as properties:
+                self.server_properties = properties.readlines()
 
 
 def get_server_by_name(server_name: str) -> MinecraftServer | None:
