98c35940395bcb95ea1eb38b237c00526ab038fd
Davide <58525106+ddavidel@users.noreply.github.com>
2024-12-20 23:24:17 +0100

    feat(translations): italian translations

---
+++ localization/it.py
@@ -0,0 +1,21 @@
+"""
+Italian translations
+"""
+
+translations = {
+    "Create Server": "Crea Server",
+    "Start": "Avvia",
+    "Settings": "Impostazioni",
+    "Console": "Terminale",
+    "Edit server settings": "Modifica impostazioni",
+    "Edit server properties": "Modifica proprietà",
+    "Delete server": "Elimina server",
+    "Your servers": "I tuoi server",
+    "Seems like you don't have any. Let's create one!": "Sembra tu non ne abbia. Creane uno!",
+    "Save": "Salva",
+    "Starting": "Avviando",
+    "Stopping": "Stoppando",
+    "Running": "In esecuzione",
+    "Stopped": "Spento",
+    "Invalid server": "Server invalido",
+}
