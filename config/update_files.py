386146e9271a2e9d6df4714500ab1cf608d4ef7a
Davide <58525106+ddavidel@users.noreply.github.com>
2024-12-27 12:19:35 +0100

    feat(update): update file list

---
+++ config/update_files.py
@@ -0,0 +1,15 @@
+"""
+Update files
+"""
+
+UPDATE_FILES = [
+    {"filename": "pages.py", "path": "/modules"},
+    {"filename": "ServerCreator.py", "path": ""},
+    # {"filename": "settings.py", "path": "/config"},
+    # {"filename": "update.py", "path": "/modules"},
+    {"filename": "server.py", "path": "/modules"},
+    {"filename": "utils.py", "path": " /modules"},
+    {"filename": "style.css", "path": " /static"},
+    {"filename": "requirements.txt", "path": ""},
+    {"filename": "VERSION.txt", "path": ""},
+]
