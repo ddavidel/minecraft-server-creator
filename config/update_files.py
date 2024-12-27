542933f35f365a46a3a1e6711dc10776a9ac2a1f
Davide <58525106+ddavidel@users.noreply.github.com>
2024-12-27 13:32:27 +0100

    add 'update.py' in update_files

--- config/update_files.py
+++ config/update_files.py
@@ -6,7 +6,7 @@ UPDATE_FILES = [
     {"filename": "pages.py", "path": "modules"},
     {"filename": "ServerCreator.py", "path": ""},
     # {"filename": "settings.py", "path": "config"},
-    # {"filename": "update.py", "path": "modules"},
+    {"filename": "update.py", "path": ""},
     {"filename": "server.py", "path": "modules"},
     {"filename": "utils.py", "path": "modules"},
     {"filename": "style.css", "path": "static"},
