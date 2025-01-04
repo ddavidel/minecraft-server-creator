a17271103561c8ceb50a7723f12cf8fd61af1a8f
Davide <58525106+ddavidel@users.noreply.github.com>
2025-01-04 15:12:23 +0100

    feat(forge server): add forge version url

--- config/settings.py
+++ config/settings.py
@@ -23,4 +23,5 @@ JAVA_BIT_MODEL = "64"
 NOGUI = True
 
 # URLS
-VERSION_LIST_URL = "https://gist.githubusercontent.com/cliffano/77a982a7503669c3e1acb0a0cf6127e9/raw/a36b1e2f05ed3f1d1bbe9a7cf7fd3cfc7cb7a3a8/minecraft-server-jar-downloads.md"
+JAVA_VERSION_LIST_URL = "https://gist.githubusercontent.com/cliffano/77a982a7503669c3e1acb0a0cf6127e9/raw/a36b1e2f05ed3f1d1bbe9a7cf7fd3cfc7cb7a3a8/minecraft-server-jar-downloads.md"
+FORGE_VERSION_LIST_URL = "https://raw.githubusercontent.com/ddavidel/minecraft-forge-links/refs/heads/main/version_list.json"
