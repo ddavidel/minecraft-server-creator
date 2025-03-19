08e6421a0c32936de26865aec6dd76495fb24a31
Davide <58525106+ddavidel@users.noreply.github.com>
2025-03-19 20:55:34 +0100

    fix(import): update import paths for MinecraftServer and utility functions

--- modules/pages.py
+++ modules/pages.py
@@ -18,7 +18,8 @@ from modules.utils import (
     open_file_explorer,
     minimize_window,
 )
-from modules.server import MinecraftServer, server_list, get_server_by_uuid
+from modules.servers.models import MinecraftServer
+from modules.servers.utils import get_server_by_uuid, server_list
 from modules.translations import translate as _
 from update import check_for_updates
 
