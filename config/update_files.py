273fb700758136371ae4e8e975e7dfeb9ebb8b8a
Davide <58525106+ddavidel@users.noreply.github.com>
2025-04-19 12:21:34 +0200

    feat(update): add Spigot module to update files configuration

--- config/update_files.py
+++ config/update_files.py
@@ -20,4 +20,5 @@ UPDATE_FILES = [
     {"filename": "utils.py", "path": "modules/servers"},
     {"filename": "models.py", "path": "modules/servers"},
     {"filename": "logger.py", "path": "modules"},
+    {"filename": "spigot.py", "path": "modules/servers"},
 ]
